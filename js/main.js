f889f08db9379d8d50cd6e8bdc60908b26c38c3f
Krizia <krizialudovico@gmail.com>
2018-05-07 22:14:18 -0700

    uploaded

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// function name(promptType) {
+	var firstName = prompt("Enter your first name:");
+	var lastName = prompt("Enter your last name:");
+	var age = prompt("Enter your age:");
+
+	alert("Hello, " + firstName + " " + lastName + "!")
+// 	if (person) {
+	   
+// 	}
+
+// }
+
+// name("first name:");
+// name("last name:");
+// name("age:");
