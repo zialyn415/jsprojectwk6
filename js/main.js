7abf37d0a54ca511412247b230785d4bfbc48eb2
Krizia <krizialudovico@gmail.com>
2018-05-09 07:43:27 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-// function name(promptType) {
-	var firstName = prompt("Enter your first name:");
-	var lastName = prompt("Enter your last name:");
-	var age = prompt("Enter your age:");
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var getAge = prompt("Enter your age:");
 
-	alert("Hello, " + firstName + " " + lastName + "!")
-// 	if (person) {
-	   
-// 	}
+alert("Hello, " + firstName + " " + lastName + "!" + " " + "This is your age in months: " + ageInMonths(getAge)+ "." + " " + "This is your age in days: " + ageInDays(getAge));
 
-// }
+function ageInDays(age) {
+	return age * 365;
+}
 
-// name("first name:");
-// name("last name:");
-// name("age:");
+function ageInMonths(age) {
+	return age * 12;
+}
+
+// es6 -- what is let?
